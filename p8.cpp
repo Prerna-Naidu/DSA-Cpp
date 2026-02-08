b7234154325e0d2afdf21a819dd6039bdc9b4035
Prerna-Naidu <prerna.naidu11@gmail.com>
2026-02-08 11:56:38 +0530

    Modified the patterns

--- p8.cpp
+++ p8.cpp
@@ -4,10 +4,10 @@ int main(){
     int n=4;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n-i;j++){
-            cout<<" ";
+            cout<<"  ";
         }
         for(int k=1;k<=i;k++){
-            cout<<"*";
+            cout<<"* ";
         }
         cout<<endl;
     }
